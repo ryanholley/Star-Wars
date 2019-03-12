d5e757878889c5f649c7f9530e571df9821acbc5
Ryan Holley <ryanlholley@gmail.com>
2019-03-12 11:29:20 -0700

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { StarWarsService } from './star-wars.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent implements OnInit {
+  // title = 'star-wars-project';
+
+  swService: StarWarsService;
+
+  constructor(swService: StarWarsService) {
+    this.swService = swService;
+  }
+
+ngOnInit() {
+  this.swService.fetchCharacters();
+}
+}
